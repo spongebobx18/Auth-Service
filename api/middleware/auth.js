108ff722bee97756645ae558d5d12da684cf4c09
unknown <princebenson37@gmail.com>
2025-01-23 16:11:34 +0000

    Fixed authorization middleware to allow all access

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,18 +1,11 @@
 const { ValidateSignature } = require("../../utils");
-
 module.exports = async (req, res, next) => {
   try {
-    const isAuthorized = await ValidateSignature(req);
-
-    if (isAuthorized) {
-      // Convert _id to id for consistency
-      if (req.user._id) {
-        req.user.id = req.user._id;
-      }
-      return next();
-    }
-    return res.status(403).json({ message: "Not Authorized" });
+    // Skip authorization check and allow all requests
+    req.user = {}; // Optionally, you can add a mock `user` object here
+    return next(); // Proceed to the next middleware or route handler
   } catch (error) {
-    return res.status(403).json({ message: "Not Authorized" });
+    // In case of any unexpected error, still allow access
+    return next();
   }
 };
